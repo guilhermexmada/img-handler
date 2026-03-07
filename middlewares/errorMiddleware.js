da7260264e33816f448f3e21f81fb5d58ebe984c
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-03-06 22:30:55 -0300

    feature : verificação de tipo e tamanho do arquivo

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,6 +1,13 @@
 function errorMiddleware(err, req, res, next) {
     console.error("Error:", err.message)
 
+    if (err.code === "LIMIT_FILE_SIZE"){ // trata erro de arquivo muito grande
+        return res.status(400).json({
+            success: false,
+            error: "Arquivo excede limite de 5MB"
+        })
+    }
+
     res.status(err.status || 500).json({
         success: false,
         error: err.message || "Erro interno do servidor"
--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -20,6 +20,25 @@ const storage = multer.diskStorage({ // salva arquivo no servidor (pasta uploads
     }
 })
 
-const upload = multer({storage}) // cria middleware configurado
+// tipos permitidos
+const allowedMimeTypes = [
+    'image/jpeg',
+    'image/png',
+    'image/webp'
+]
+
+const fileFilter = (req, file, cb) => {
+    if(allowedMimeTypes.includes(file.mimetype)){ // se algum dos tipos permitidos é o tipo do arquivo
+        cb(null, true)
+    } else {
+        cb(new Error('Tipo de arquivo não permitido'), false)
+    }
+}
+
+const upload = multer({ // cria middleware configurado
+    storage,
+    fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 } // máx de 500mb
+})
 
 export default upload // exporta para usar nas rotas
