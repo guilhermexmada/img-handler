87bbf6b582f81ef111b08f39c90d0afc39a03c5c
Shimada <guilhermeshimadapereira@gmail.com>
2026-03-10 18:11:24 -0300

    chore : configurando conexão com banco usando sequelize

---
+++ config/database.js
@@ -0,0 +1,27 @@
+import { Sequelize } from 'sequelize'
+
+// verifica se as variáveis de ambiente foram definidas
+if(!process.env.DB_HOST || !process.env.DB_NAME){
+    throw new Error('Variáveis de ambiente do banco de dados não foram definidas.')
+}
+
+const sequelize = new Sequelize(
+    // autentica-se no banco com variáveis de ambiente
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: true, // liga/desliga logs de queries no console
+        timezone: '-03:00',
+        pool:{ // define quantidade de conexões simultâneas permitidas com o banco
+            max: 10,
+            min: 0, // conforme o necessário
+            acquire: 30000, // tempo de espera para obter uma conexão
+            idle: 10000 // tempo de espera para uma conexão ficar inativa
+        }
+    }
+)
+
+export default sequelize
