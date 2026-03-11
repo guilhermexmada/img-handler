1eaaa7945275d285b2997b87b8c7cff6d955a8b5
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-03-10 22:43:43 -0300

    chore : inicialização integrada de banco, storage e servidor (bootstrap)

--- server.js
+++ server.js
@@ -1,30 +1,56 @@
 import app from './index.js'
+import sequelize from './config/database.js'
+import { initDatabase } from './database/init.js'
+import { initStorage } from './storage.js'
+
 
 const PORT = process.env.PORT || 8080
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`)
-})
+// bootstrap = processo que inicializa aplicação
+async function startServer() { 
+  try {
+    // inicializa banco
+    await initDatabase()
 
-server.on('error', (err) => {
-    console.error(`Erro ao iniciar o servidor: ${err}`)
+    // inicializa pasta uploads
+    await initStorage()
+
+    // inicializa servidor
+    const server = app.listen(PORT, () => {
+      console.log(`Servidor rodando em http://localhost:${PORT}`)
+    })
+
+    server.on('error', (err) => {
+      console.error(`Erro ao iniciar o servidor: ${err}`)
+      process.exit(1)
+    })
+
+    server.on('uncaughtException', (err) => {
+      console.error(`Uncaught exception: ${err}`)
+      process.exit(1)
+    })
+
+    process.on('unhandledRejection', (reason) => {
+      console.error('Unhandled Rejection:', reason);
+      process.exit(1);
+    });
+
+    process.on('SIGTERM', async () => {
+      console.log('🛑 Encerrando servidor...');
+
+      await sequelize.close() // fecha conexão com banco
+
+      server.close(() => {
+        console.log('✅ Servidor encerrado corretamente');
+        process.exit(0);
+      });
+    });
+  } catch (error) {
+    console.error('Falha ao iniciar a aplicação:', error)
     process.exit(1)
-})
+  }
+}
 
-server.on('uncaughtException', (err) => {
-    console.error(`Uncaught exception: ${err}`)
-    process.exit(1)
-})
+// executando inicialização
+startServer()
 
-process.on('unhandledRejection', (reason) => {
-  console.error('Unhandled Rejection:', reason);
-  process.exit(1);
-});
-
-process.on('SIGTERM', () => { 
-  console.log('🛑 Encerrando servidor...');
-  server.close(() => {
-    console.log('✅ Servidor encerrado corretamente');
-    process.exit(0);
-  });
-});
