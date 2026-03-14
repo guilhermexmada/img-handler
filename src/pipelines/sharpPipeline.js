97ebdfa9c13b7e339dbbc07621180ac75ab7ed4b
Shimada <guilhermeshimadapereira@gmail.com>
2026-03-14 17:25:21 -0300

    feature : finalizando pipeline de processamento em sharp

--- src/pipelines/sharpPipeline.js
+++ src/pipelines/sharpPipeline.js
@@ -5,36 +5,50 @@ import sharp from 'sharp'
     outputPath = caminho para salvar imagem resultante
     operations = objeto contendo as transformações
 */
-export async function sharPipeline(inputPath, outputPath, operations = {}){
+export async function sharPipeline(inputPath, outputPath, operations = {}) {
     // cria pipeline lazy (operações são executadas apenas quando uma função de saída é chamada)
     let pipeline = sharp(inputPath)
 
-    if(operations.resize){ // se existe a operação X no objeto operations passado como parâmetro
+    if (operations.resize) { // se existe a operação X no objeto operations passado como parâmetro
         const width = operations.resize.width
         const height = operations.resize.height
         pipeline = pipeline.resize(width, height) // sempre altera a própria imagem na variável pipeline e passa para próxima operação
     }
 
-    if(operations.rotate){
+    if (operations.rotate) {
         const degrees = operations.rotate
         pipeline = pipeline.rotate(degrees)
     }
 
-    if(operations.crop){
+    if (operations.crop) {
         const dimensions = operations.crop
         pipeline = pipeline.crop(dimensions)
     }
 
-    if(operations.format){
+    if (operations.format) {
         const format = operations.format
         pipeline = pipeline.format(format)
+    } else {
+        const format = 'png'
     }
 
-    if(operations.compress){
-        if(format === 'jpeg'){}
-        else if(format === 'jpeg'){}
-        else if(format === 'jpeg'){}
-        else{}
+    if (operations.compress) {
+        const quality = operations.compress
+        if (format === 'jpeg') {
+            pipeline = pipeline.jpeg({ quality })
+        }
+        else if (format === 'png') {
+            pipeline = pipeline.png({ quality })
+        }
+        else if (format === 'webp') {
+            pipeline = pipeline.webp({ quality })
+        }
+        else {
+            pipeline = pipeline.png({ quality })
+        }
     }
+
+    await pipeline.toFile(outputPath) // gera arquivo no caminho de destino
+    return outputPath // retorna os dados da imagem processada
 
 }
