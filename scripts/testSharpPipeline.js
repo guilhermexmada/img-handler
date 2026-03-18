840be83a20209a155e537de80ab056e4705f8046
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-03-18 20:48:35 -0300

    feature : criando pasta storage/processed/ para resultados da pipeline e utilizando no script de teste

--- scripts/testSharpPipeline.js
+++ scripts/testSharpPipeline.js
@@ -5,26 +5,26 @@ import fs from 'fs'
 // função de teste executada com 'node nome-do-arquivo'
 async function runTest() {
 
-    const fileName = 'f43f3fa4-8639-4c9e-99fd-0ccae89004f2.jpg' // troque 'test.png' pelo nome do arquivo de teste
+    const fileName = '67a4c7f0-61b6-46a6-b56c-84cdfca356f4.png' // troque 'test.png' pelo nome do arquivo de teste
 
     // definindo caminho da imagem original
     const inputPath = path.resolve(`./storage/uploads/${fileName}`)
 
     // definindo caminho para salvar imagem resultante
-    const firstOutputPath = path.resolve('./storage/test-output.png')
+    const firstOutputPath = path.resolve('./storage/processed/test-output.png')
 
     // se o caminho já existe, obtém um novo caminho de saída
     const outputPath = await getUniquePath(firstOutputPath)
 
     // define operações da pipeline
     const operations = {
-        //resize: { width: 100, heigth: 100},
-        //rotate: 90,
-        //crop: { width: 25, height: 25, top: 0, left: 0},
+        // resize: { width: 100, heigth: 100},
+        // rotate: 90,
+        // crop: { width: 25, height: 25, top: 0, left: 0},
         compress: { quality: 1},
         format: 'png'
     }
-
+    console.log(inputPath)
     // tenta executar pipeline
     try {
         const result = await sharPipeline(inputPath, outputPath, operations)
@@ -49,7 +49,7 @@ async function getUniquePath(outputPath){
     // loop que dura até encontrar um nome de arquivo único
     while (true){
         try {
-            console.log(dir)
+            //console.log(dir)
             await fs.promises.access(finalPath) // tenta acessar o caminho de saída atual
             finalPath = path.join(`${dir}/${name}(${i})${ext}`) // se conseguir, cria um novo caminho de saída
             i++ // incrementa contador para tentar novamente
