43e2900b1de4b89e6957b813ab63c722e6a677b9
Shimada <guilhermeshimadapereira@gmail.com>
2026-03-24 23:56:54 -0300

    fix : tratando erros e corrigindo responsabilidades na rota para processar imagem

--- src/controllers/processController.js
+++ src/controllers/processController.js
@@ -1,34 +1,34 @@
 import processService from "../services/processService.js"
 import uploadService from "../services/uploadService.js"
 import path from 'path'
+import AppError from "../utils/appError.js"
 
 class ProcessController {
     async processImage(req, res, next){
         try {
-            // console.log('chegou no controller')
-            
             // captura id da imagem original e operações para aplicar nela
-            const originalImageId = req.body.id
+            const originalId = req.body.id
             const operations = req.body.operations
-            // console.log(req.body.id)
+            
+            if(!originalId || !operations){
+                throw new AppError('Campos obrigatórios não preenchidos', 400) // id ou operações não foram passados para controller
+            }
 
             // procura imagem original no banco e captura caminho
-            const originalImage = await uploadService.getOne(originalImageId)
-            //  console.log(originalImage)
+            const originalImage = await uploadService.getOne(originalId)
 
             // se a imagem original existe, executa a pipeline
-            if(originalImage.result.exists_in_storage === true){
-                // console.log('imagem existe')
+            if(originalImage.exists_in_storage === true){
                 // captura caminho e nome dela
-                const originalImageData = originalImage.result.saved_image.dataValues
+                const originalImageData = originalImage.saved_image
                 const filePath = originalImageData.file_path
                 const parsedPath = path.parse(filePath)
                 const fileName = parsedPath.name
                 // console.log('dados extraidos')
                 const result = await processService.sendToPipeline(fileName, filePath, operations)
-                res.status(200).json({message: 'Imagem processada com sucesso!', result})
+                res.status(200).json({success: true, message: 'Imagem processada com sucesso', result})
             } else {
-                res.status(404).json({error : 'Imagem original não encontrada.'})
+                throw new AppError('Imagem original não encontrada', 404) // imagem não existe na storage
             }
 
         } catch (error) {
