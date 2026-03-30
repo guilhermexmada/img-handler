15ef7731d45f3004680281fe2a323c4d90f2f54e
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-03-30 20:53:40 -0300

    fix : excluindo campo body no controller para renderizar a página home.ejs

--- src/controllers/viewController.js
+++ src/controllers/viewController.js
@@ -3,7 +3,6 @@ class viewController{
     renderHome(req, res){
         res.render('pages/home',{
             title: 'Home', // título na aba da página
-            body: '<h1> Bem-vindo à homepage </h1>' // conteúdo da página (dentro de <main></main>)
         })
     }
 }   
